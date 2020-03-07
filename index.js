4d162b7d3fc9920995d986ba214ceb9ba5616899
Jeremy <me@jeremypoole.ca>
2020-03-07 00:41:28 -0800

    Prototype customer support server with seperate staff and customer clients. Customer client can send/receive to staff, staff client can reply to any connected customer.

--- index.js
+++ index.js
@@ -3,16 +3,61 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.get('/client', function(req, res){
+  res.sendFile(__dirname + '/public/client.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+app.get('/staff', function(req, res){
+  res.sendFile(__dirname + '/public/staff.html');
 });
 
-http.listen(port, function(){
+let adminId = null
+
+const staffNS = io.of('/staff')
+const clientNS = io.of('/client')
+
+/**
+ * 
+ * ChatEvent {
+ *   to: String,
+ *   ?from: String,
+ *   message: String
+ * }
+ * 
+ */
+
+staffNS.on('connection', function(socket) {
+  adminId = socket.id
+
+  socket.on('chatEvent', function(chatEvent) {
+    // console.log('chat event on staff')
+    if (chatEvent.to === 'staff') {
+      staffNS.emit('chatEvent', chatEvent)
+    } else {
+      // console.log('bridge message')
+      clientNS.in(chatEvent.to).emit('chatEvent', chatEvent)
+    }
+  })
+
+  socket.on('connectionEvent', function(id) {
+    staffNS.emit('chatEvent', { to: 'staff', from: id, message: `New connection from ${id}` })
+  })
+
+  socket.on('disconnect', function() {
+    adminId = null
+  })
+})
+
+clientNS.on('connection', function(socket) {
+  staffNS.emit('connectionEvent', socket.id)
+
+  socket.on('chatEvent', function(chatEvent) {
+    chatEvent.from = socket.id
+    staffNS.emit('chatEvent', chatEvent)
+  })
+
+})
+
+http.listen(port, '0.0.0.0', function(){
   console.log('listening on *:' + port);
 });
